19f959c44a363f30e0e096d9975b4c89d508d30d
Anderson Mendes <mendes.ams@gmail.com>
2023-02-28 23:33:39 -0300

    Iniciando e estruturando projeto.

---
+++ src/test/java/com/anderson/mendes/EventosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anderson.mendes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
