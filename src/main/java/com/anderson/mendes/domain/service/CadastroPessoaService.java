867049b50a88535f340c5237329e9890c453ffd4
Anderson Mendes <mendes.ams@gmail.com>
2023-04-22 23:15:02 -0300

    Criando método excluir na camada de serviço.

--- src/main/java/com/anderson/mendes/domain/service/CadastroPessoaService.java
+++ src/main/java/com/anderson/mendes/domain/service/CadastroPessoaService.java
@@ -1,8 +1,12 @@
 package com.anderson.mendes.domain.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import com.anderson.mendes.domain.exceptions.EntidadeEmUsoException;
+import com.anderson.mendes.domain.exceptions.EntidadeNaoEncontradaException;
 import com.anderson.mendes.domain.model.Pessoa;
 import com.anderson.mendes.domain.repository.PessoaRepository;
 
@@ -14,6 +18,20 @@ public class CadastroPessoaService {
 	
 	public Pessoa salvar(Pessoa pessoa) {
 		return pessoaRepository.save(pessoa);
+	}
+	
+	public void excluir(Long pessoaId) {
+		try {
+			pessoaRepository.deleteById(pessoaId);
+			
+		} catch (EmptyResultDataAccessException e) {
+			throw new EntidadeNaoEncontradaException(
+				String.format("Não existe um cadastro de pessoa com código %d", pessoaId));
+		
+		} catch (DataIntegrityViolationException e) {
+			throw new EntidadeEmUsoException(
+				String.format("Pessoa de código %d não pode ser removida, pois está em uso", pessoaId));
+		}
 	}
 	
 }
