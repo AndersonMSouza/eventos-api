655c8dddf5447d7a011aa2e9fd11f4961f62e206
Anderson Mendes <mendes.ams@gmail.com>
2023-04-24 21:53:50 -0300

    Refatorando método buscar.

--- src/main/java/com/anderson/mendes/api/controller/PessoaController.java
+++ src/main/java/com/anderson/mendes/api/controller/PessoaController.java
@@ -38,14 +38,8 @@ public class PessoaController {
 	}
 	
 	@GetMapping("/{pessoaId}")
-	public ResponseEntity<Pessoa> buscar(@RequestBody Long pessoaId) {
-		Optional<Pessoa> pessoa = pessoaRepository.findById(pessoaId);
-		
-		if (pessoa.isPresent()) {
-			return ResponseEntity.ok(pessoa.get());
-		}
-		
-		return ResponseEntity.notFound().build();
+	public Pessoa buscar(@RequestBody Long pessoaId) {
+		return cadastroPessoaService.buscarOuFalhar(pessoaId);
 	}
 	
 	@PostMapping
