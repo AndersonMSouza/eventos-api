2223a5ea69876871ffce17ddaf90ad4a8723c73e
Anderson Mendes <mendes.ams@gmail.com>
2023-04-27 15:43:28 -0300

    Refatorando método remover.

--- src/main/java/com/anderson/mendes/api/controller/PessoaController.java
+++ src/main/java/com/anderson/mendes/api/controller/PessoaController.java
@@ -1,12 +1,10 @@
 package com.anderson.mendes.api.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,8 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.anderson.mendes.domain.exceptions.EntidadeEmUsoException;
-import com.anderson.mendes.domain.exceptions.EntidadeNaoEncontradaException;
 import com.anderson.mendes.domain.model.Pessoa;
 import com.anderson.mendes.domain.repository.PessoaRepository;
 import com.anderson.mendes.domain.service.CadastroPessoaService;
@@ -61,17 +57,9 @@ public class PessoaController {
 	}
 	
 	@DeleteMapping("/{pessoaId}")
-	public ResponseEntity<?> remover(@PathVariable Long pessoaId) {
-		try {
-			pessoaRepository.deleteById(pessoaId);
-			return ResponseEntity.noContent().build();
-			
-		} catch (EntidadeNaoEncontradaException e) {
-			return ResponseEntity.notFound().build();
-		
-		} catch (EntidadeEmUsoException e) {
-			return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
-		}		
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long pessoaId) {
+		cadastroPessoaService.excluir(pessoaId);
 	}
 		
 }
